f66d5efb8f50ea13ca5dd2d245f98c174a15224d
Slawomir Dyk <slawomir.dyk@astrosat.net>
2023-01-27 16:15:21 +0000

    I learn more advanced hooks using the epic react platform

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,19 +1,48 @@
-// useReducer: simple Counter
-// http://localhost:3000/isolated/exercise/01.js
-
 import * as React from 'react'
+const countReducer = (state, action) => {
+  const {type, step} = action
+  switch (type) {
+    case 'increment': {
+      return {
+        ...state,
+        count: state.count + step,
+      }
+    }
+    case 'decrement': {
+      return {
+        ...state,
+        count: state.count - step,
+      }
+    }
+    case 'reset': {
+      return {
+        ...state,
+        count: state.count - state.count,
+      }
+    }
+    default: {
+      throw new Error(`Unsupported action type: ${type}`)
+    }
+  }
+}
 
-function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+function Counter({initialCount = 0, step = 2}) {
+ 
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
-  return <button onClick={increment}>{count}</button>
+  const {count} = state
+  const increment = () => dispatch({type: 'increment', step})
+  const decrement = () => dispatch({type: 'decrement', step})
+  const reset = () => dispatch({type: 'reset', step})
+  return (
+    <>
+      <button onClick={increment}>{count}</button>
+      <button onClick={decrement}>{count}</button>
+      <button onClick={reset}>Reset</button>
+    </>
+  )
 }
 
 function App() {
